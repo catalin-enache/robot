dddbd38b2bfee963fdc1e2ae2fbb4fb6bedf188c
Enache Catalin <enachecatalin@Catalins-MacBook-Pro-4.local>
2020-03-14 18:54:03 +0200

    Updates

---
+++ precache-manifest.f676c9da21b3d8a03fe3d7dbf1d02465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0544fbe9390a1001a465ad9307916423",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "62f913005aeb75184930",
+    "url": "/robot/static/css/main.ccad2760.chunk.css"
+  },
+  {
+    "revision": "1bb0dc9d8408cb197dca",
+    "url": "/robot/static/js/2.ca1e7e46.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robot/static/js/2.ca1e7e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f913005aeb75184930",
+    "url": "/robot/static/js/main.5c970eeb.chunk.js"
+  },
+  {
+    "revision": "8e39efa505ac2d0a8c44",
+    "url": "/robot/static/js/runtime-main.f352755f.js"
+  }
+]);
