d6243c24b1ab431f6cc0dcbf7fd05a6ae4fddc2b
Enache Catalin <enachecatalin@Catalins-MacBook-Pro-4.local>
2020-03-14 18:42:34 +0200

    added github pages

---
+++ docs/precache-manifest.9b7a5175fb25855c4c11646a6952a22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0a9daf591d1e00850d085bb7f17cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62f913005aeb75184930",
+    "url": "/static/css/main.ccad2760.chunk.css"
+  },
+  {
+    "revision": "1bb0dc9d8408cb197dca",
+    "url": "/static/js/2.ca1e7e46.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ca1e7e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f913005aeb75184930",
+    "url": "/static/js/main.5c970eeb.chunk.js"
+  },
+  {
+    "revision": "a19f041d448f568f634e",
+    "url": "/static/js/runtime-main.f01d70fc.js"
+  }
+]);
